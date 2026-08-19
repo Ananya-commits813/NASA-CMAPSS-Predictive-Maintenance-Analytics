da0142eb5bd96df42bec7e000a9465057c43945b
Ananya Roy <ananya.roy3467@gmail.com>
2026-08-19 09:40:22 +0530

    Update llm.py

--- Nlp to Sql Ai Agent/llm.py
+++ Nlp to Sql Ai Agent/llm.py
@@ -17,7 +17,8 @@ def generate_sql_from_question(natural_language_query,selected_table):
     try :
        schema_description = get_schema_from_database(selected_table)
        api_key = st.secrets["Api_key"]
-       client = genai.Client(api_key=api_key)
+       client = Groq(api_key=api_key)
+
        prompt = SQL_GENERATION_PROMPT.format(
        selected_table=selected_table,
        schema_description=schema_description,
@@ -34,6 +35,10 @@ def generate_sql_from_question(natural_language_query,selected_table):
         ],
         temperature=0
         )
+        # Extract generated SQL
+        sql_query = response.choices[0].message.content.strip()
+
+        return sql_query
 
     
     except exceptions.ResourceExhausted:
